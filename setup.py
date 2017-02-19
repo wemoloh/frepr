a0a33efdc6df53301966c9240e5f534ac6bf1426
Bart Robinson <bart@bartrobinson.com>
2017-02-19 16:57:47 -0700

    tweaks for first PyPI release

--- setup.py
+++ setup.py
@@ -7,16 +7,20 @@ DBLCONV = 'double-conversion/double-conversion'
 sources = glob(join(DBLCONV, '*.cc'))
 sources.append('frepr.cpp')
 
+with open('README.rst') as fin:
+    readme = fin.read()
+
 setup(
     name='frepr',
-    version='0.1.0',
+    version='0.1.1',
     description='Fast float repr',
+    long_description=readme,
     author='Bart Robinson',
     author_email='bart@bartrobinson.com',
     license='BSD 3-Clause License',
     url='https://github.com/wemoloh/frepr',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2',
