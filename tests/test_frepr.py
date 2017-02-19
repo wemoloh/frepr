c8879e7d8a5a8202b938331203ad87d37c6622eb
Bart Robinson <bart@bartrobinson.com>
2017-02-19 01:47:01 -0700

    fix for tests under Python 2.6

--- tests/test_frepr.py
+++ tests/test_frepr.py
@@ -1,6 +1,7 @@
 from ast import literal_eval
 from random import getrandbits
 from struct import pack, unpack
+import sys
 
 import frepr
 
@@ -22,22 +23,29 @@ def test_random():
         d = random_double()
         r = repr(d)
         f = frepr.frepr(d)
+        
         if r in ('inf', '-inf', 'nan'):
             assert r == f
         else:
             assert d == literal_eval(f)
-        assert len(r) >= len(f)
+        
+        # Shortest float repr was added in Python 2.7; before that, floats
+        # could actually be shorter with the native repr because it wouldn't
+        # use an exponent (example: '-68547676833205240.0').
+        if sys.version_info >= (2, 7):
+            assert len(r) >= len(f)
 
 def test_install():
     # Normal repr adds a leading zero to the exponent; frepr does not.
-    assert repr(1e-6) == '1e-06'
+    assert 'e-0' in repr(1e-6)
     
     ds = [random_double() for _ in range(10000)]
     rs = [repr(d) for d in ds]
     
     frepr.install()
     try:
-        assert repr(1e-6) == '1e-6'
+        r = repr(1e-6)
+        assert 'e-' in r and 'e-0' not in r
         fs = [repr(d) for d in ds]
     finally:
         frepr.uninstall()
